df819a5148b159bd4362d0cbbc00f5b1d1657822
ktw3577 <taewongim06@gmail.com>
2023-01-20 07:41:53 +0900

    config수정

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -2,10 +2,12 @@ module.exports = {
   reactStrictMode: true,
   images: {
     formats: ["image/webp"],
+    unoptimized: true,
   },
   compiler: {
     styledComponents: true,
   },
+
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.target = "electron-renderer";

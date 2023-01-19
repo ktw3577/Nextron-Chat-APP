032dae1396f17dc41f17352534f5fe7c9c3d8052
ktw3577 <taewongim06@gmail.com>
2023-01-20 07:26:26 +0900

    임시 홈화면 수정

--- renderer/pages/home.tsx
+++ renderer/pages/home.tsx
@@ -2,20 +2,25 @@ import React from "react";
 import Head from "next/head";
 import Link from "next/link";
 import styled from "styled-components";
-import { CHAT, LOGIN, NEXT, SIGN_UP } from "../constants/routes";
+import { CHAT, LOGIN, SIGN_UP } from "../constants/routes";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebaseconfig";
 
 export default function Home() {
+  const trySignOut = () => {
+    try {
+      signOut(auth);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <Layout>
       <Head>
         <title>Home - Nextron (with-typescript)</title>
       </Head>
       <Container>
-        <LinkItem>
-          <StyledLink href={NEXT}>
-            <Text>NEXT페이지로 이동</Text>
-          </StyledLink>
-        </LinkItem>
         <LinkItem>
           <Link href={LOGIN}>
             <Text>로그인 페이지로 이동</Text>
@@ -27,10 +32,13 @@ export default function Home() {
           </Link>
         </LinkItem>
         <LinkItem>
-          <Link href={CHAT}>
+          <Link href={"/chat/default"}>
             <Text>채팅창으로 이동</Text>
           </Link>
         </LinkItem>
+        <SignOutContainer>
+          <SignOut onClick={trySignOut}>로그아웃</SignOut>
+        </SignOutContainer>
       </Container>
     </Layout>
   );
@@ -50,4 +58,7 @@ const LinkItem = styled.div`
 const Text = styled.a`
   color: blue;
 `;
-const StyledLink = styled(Link)``;
+const SignOutContainer = styled.div`
+  margin-top: 5px;
+`;
+const SignOut = styled.div``;

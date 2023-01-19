5c1914e47f6eb25a0221dbb81e2a0d368c866d11
ktw3577 <taewongim06@gmail.com>
2023-01-20 07:25:28 +0900

    에러 수정

--- renderer/next.config.js
+++ renderer/next.config.js
@@ -3,6 +3,9 @@ module.exports = {
   images: {
     formats: ["image/webp"],
   },
+  compiler: {
+    styledComponents: true,
+  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.target = "electron-renderer";

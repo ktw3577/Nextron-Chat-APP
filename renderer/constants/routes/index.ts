9ecae0e66ef199546535abdb285a04afd339e64b
ktw3577 <taewongim06@gmail.com>
2023-01-20 07:26:41 +0900

    rotue추가

--- renderer/constants/routes/index.ts
+++ renderer/constants/routes/index.ts
@@ -1,5 +1,4 @@
-export const HOME = "/";
+export const HOME = "/home";
 export const LOGIN = "/login";
-export const CHAT = "/chat";
+export const CHAT = "/chat/default";
 export const SIGN_UP = "/sign-up";
-export const NEXT = "/next";

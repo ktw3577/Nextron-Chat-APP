7135975263c0f1d6d3120719f3537b628494e8ce
ktw3577 <taewongim06@gmail.com>
2023-01-20 07:27:16 +0900

    theme적용

---
+++ renderer/pages/_app.tsx
@@ -0,0 +1,11 @@
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "styled-components";
+import theme from "../styles/theme";
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
